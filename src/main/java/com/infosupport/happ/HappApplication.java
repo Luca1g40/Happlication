c8395aa59455f1d2c86f19dc888e9055b19158c5
LucaJ <Luca.Franken@student.hu.nl>
2022-09-21 20:01:15 +0200

    Heroku deployment test

--- src/main/java/com/infosupport/happ/HappApplication.java
+++ src/main/java/com/infosupport/happ/HappApplication.java
@@ -3,12 +3,18 @@ package com.infosupport.happ;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
-public class HappApplication {
+@SpringBootApplication
+public class HappApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(HappApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HappApplication.class);
+    }
 }
