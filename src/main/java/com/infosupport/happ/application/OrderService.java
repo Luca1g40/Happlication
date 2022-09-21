37aa2e6802d5643baf982f382749c99d1905ceff
maarten <maarten.kuijf@student.hu.nl>
2022-09-21 12:09:06 +0200

    Order exception, delete added

--- src/main/java/com/infosupport/happ/application/OrderService.java
+++ src/main/java/com/infosupport/happ/application/OrderService.java
@@ -5,6 +5,7 @@ import com.infosupport.happ.data.OrderRepository;
 import com.infosupport.happ.domain.Order;
 import com.infosupport.happ.domain.PreperationStatus;
 import com.infosupport.happ.domain.Table;
+import com.infosupport.happ.domain.exceptions.ItemNotFound;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -21,21 +22,32 @@ public class OrderService {
     }
 
     public OrderData createOrder(Long id, PreperationStatus status){
-        Table table = this.tableService.getTable(id); //TODO: Exception geen bestaand id toevoegen
+        Table table = this.tableService.getTable(id);
         Order order = orderRespository.save(new Order(table, LocalDateTime.now(), status, new ArrayList<>()));
         return createOrderData(order);
     }
 
-    public Order getOrder(Long id){
-        return this.orderRespository.findById(id)
-                .orElseThrow(); //TODO: Exception toevoegen
+    public OrderData getOrder(Long id){
+        if (!orderExists(id)){
+            throw new ItemNotFound("order");
+        }else {
+            Order order = orderRespository.getById(id);
+            return new OrderData(order.getTableNr(), order.getTimeOfOrder(), order.getPreperationStatus(), order.getProducts());
+        }
+    }
+
+    public void deleteOrder(Long id) {
+        orderRespository.deleteById(id);
     }
 
     private OrderData createOrderData(Order order){
         return new OrderData(order.getTableNr(),
                 order.getTimeOfOrder(),
                 order.getPreperationStatus(),
-                order.getProducts(),
-                order.getId());
+                order.getProducts());
+    }
+
+    private boolean orderExists(Long id){
+        return orderRespository.existsById(id);
     }
 }
--- src/main/java/com/infosupport/happ/presentation/OrderController.java
+++ src/main/java/com/infosupport/happ/presentation/OrderController.java
@@ -3,13 +3,16 @@ package com.infosupport.happ.presentation;
 import com.infosupport.happ.application.OrderService;
 import com.infosupport.happ.application.dto.OrderData;
 import com.infosupport.happ.domain.Order;
+import com.infosupport.happ.domain.exceptions.ItemNotFound;
 import com.infosupport.happ.presentation.dto.OrderRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/happ")
 public class OrderController {
-    private OrderService orderService;
+    private final OrderService orderService;
 
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
@@ -21,7 +24,16 @@ public class OrderController {
     }
 
     @GetMapping("/order/{id}")
-    public Order getOrder(@PathVariable Long id){
-        return orderService.getOrder(id);
+    public OrderData getOrder(@PathVariable Long id){
+        try {
+            return orderService.getOrder(id);
+        }catch (ItemNotFound exception){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, exception.getMessage());
+        }
+    }
+
+    @DeleteMapping("/order/{orderid}")
+    private void deleteOrder(@PathVariable("orderid")Long orderId ) {
+        this.orderService.deleteOrder(orderId);
     }
 }
