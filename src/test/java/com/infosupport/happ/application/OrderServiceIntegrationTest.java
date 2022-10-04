79f4eabb49b03601a3cc9bc247f26c0b0bfbd1c4
maarten <maarten.kuijf@student.hu.nl>
2022-10-04 10:59:09 +0200

    Order intergrationtest added

--- src/test/java/com/infosupport/happ/application/OrderServiceIntegrationTest.java
+++ src/test/java/com/infosupport/happ/application/OrderServiceIntegrationTest.java
@@ -3,10 +3,7 @@ package com.infosupport.happ.application;
 import com.infosupport.happ.application.dto.OrderData;
 import com.infosupport.happ.data.OrderAssistant;
 import com.infosupport.happ.data.OrderRepository;
-import com.infosupport.happ.domain.Product;
-import com.infosupport.happ.domain.ShoppingCart;
-import com.infosupport.happ.domain.Table;
-import com.infosupport.happ.domain.TableStatus;
+import com.infosupport.happ.domain.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -38,22 +35,47 @@ public class OrderServiceIntegrationTest {
 
     }
 
-//    @Test
-//    @DisplayName("Order can be created")
-//    void CreateNewOrder() {
-//
-//        Table table = new Table(new ArrayList<>(), LocalTime.now(), LocalTime.now(), 4, 3, TableStatus.OCCUPIED, new ShoppingCart());
-//        Product product = new Product();
-//        List<Product> products = new ArrayList<Product>() {
-//            {
-//                add(product);
-//            }
-//        };
-//        when(orderAssistant.getTable(anyLong())).thenReturn(table);
-//        when(orderAssistant.existsById(anyLong())).thenReturn(true);
-//
-//        OrderData orderData = orderService.createOrder(1L);
-//
-//        assertEquals(UNCLAIMED, orderData.preperationStatus);
-//    }
+    @Test
+    @DisplayName("Order can be created")
+    void CreateNewOrder() {
+
+        Table table = new Table(new ArrayList<>(), LocalTime.now(), LocalTime.now(), 4, 3, TableStatus.OCCUPIED, new ShoppingCart());
+        Product product = new Product();
+        List<Product> products = new ArrayList<>() {
+            {
+                add(product);
+            }
+        };
+        when(orderAssistant.getTable(anyLong())).thenReturn(table);
+        when(orderAssistant.existsById(anyLong())).thenReturn(true);
+
+        OrderData orderData = orderService.createOrder(1L, products);
+
+        assertEquals(UNCLAIMED, orderData.preperationStatus);
+    }
+
+    @Test
+    @DisplayName("Ordered products are split in food and drinks")
+    void orderSplitFoodDrinks() {
+
+        Table table = new Table(new ArrayList<>(), LocalTime.now(), LocalTime.now(), 4, 3, TableStatus.OCCUPIED, new ShoppingCart());
+        Product drinkProduct = new Product("cola", new ArrayList<>() , ProductCategory.DRINKS,5);
+        Product foodProduct = new Product("Sushi Roll", new ArrayList<>() , ProductCategory.MAIN_COURSE,5);
+        Product foodProduct2 = new Product("Soup", new ArrayList<>() , ProductCategory.STARTER,5);
+        List<Product> products = new ArrayList<>() {
+            {
+                add(drinkProduct);
+                add(foodProduct);
+                add(foodProduct2);
+            }
+        };
+
+        when(orderAssistant.getTable(anyLong())).thenReturn(table);
+        when(orderAssistant.existsById(anyLong())).thenReturn(true);
+
+        OrderData orderData = orderService.createOrder(1L, products);
+
+        assertEquals(1, orderData.drinkProducts.size());
+        assertEquals(2, orderData.foodProducts.size());
+    }
 }
