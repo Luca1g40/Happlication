aa29704e32070c38fb41fa2bfa4c90c2bdc75c57
LucaJ <Luca.Franken@student.hu.nl>
2022-10-12 15:39:05 +0200

    Keuken frontend Orders kunnen geclaimed worden in de frontend. Meerdere orders kunnen geselecteerd worden. een order kan na het selecteren ook weer ongeselecteerd worden.
    TOEVOEGEN:
    Style setten op click event zodat je kan zien wat geselecteerd is en wat niet.

--- src/frontend/frontendhapp/src/components/OrdersDataFetching.js
+++ src/frontend/frontendhapp/src/components/OrdersDataFetching.js
@@ -5,58 +5,75 @@ function OrdersDataFetching() {
     const [orders, setOrders] = useState([])
     const [selectedOrders, setSelectedOrders] = useState([])
     const [doneSelecting, setDoneSelecting] = useState(false)
+    const [isActive, setIsActive] = useState(false);
 
     useEffect(() => {
-        console.log(doneSelecting)
         axios.get("http://localhost:8080/happ/orders")
             .then(res => {
                 console.log(res)
                 setOrders(res.data)
-                setDoneSelecting(false);
             })
             .catch(err => {
                 console.log(err)
             })
     }, [doneSelecting])
 
+    const changeStyle = event => {
+        if (event.currentTarget.style.borderStyle) {
+            event.currentTarget.style.borderStyle = null;
+        } else {
+            event.currentTarget.style.borderStyle = "outset"
+        }
+    };
+
     function claimOrder(orders){
-        axios.post(`http://localhost:8080/happ/orders/claim`,{
-            id:"86",
-            "orders" : orders
-        }) //TODO Een staffmember moet de claim maken
+        console.log(orders)
+        axios.post(`http://localhost:8080/happ/staff/86/claim`,{ //TODO Een random staffmember moet de claim maken niet 86
+            "selectedOrders" : orders
+        })
             .then(res => {
-                console.log(res)})
+                console.log(res)
+                setDoneSelecting(false)
+                setSelectedOrders([])})
             .catch(err => {
                 console.log(err)
             })
     }
 
     const addOrders = (selectedOrder) => {
-        setSelectedOrders(state => [...state, selectedOrder])
+        const index = selectedOrders.indexOf(selectedOrder);
+        if (index > -1) {
+            selectedOrders.splice(index, 1);
+        } else {
+            setSelectedOrders(state => [...state, selectedOrder])
+        }
     }
 
 
     return (
-     <div className={"grid-container"}>
-        {
-            orders.map((order, i) =>
-                <div key={order.id} className={"grid-item grid-item"+i} onClick={() => addOrders(order.id)}>
-                    <div className={"order-item"}>
-                        <p className={"table-number"}>{order.tableNr}</p>
-                        {order.foodProducts.map(product =>
-                            <p key={product.id} className={"order-products"}>{product.name}</p>
-                        )}
+        <div>
+         <div className={"grid-container"}>
+            {
+                orders.map((order, i) =>
+                    <div key={order.id} className={"grid-item grid-item"+i} onClick={() => {addOrders(order.id)}}>
+                        <div className={"order-item"}>
+                            <p className={"table-number"}>{order.tableNr}</p>
+                            {order.foodProducts.map(product =>
+                                <p key={product.id} className={"order-products"}>{product.name}</p>
+                            )}
+                        </div>
+                        <div className={"order-time"}>
+                            <p> {order.orderTime} {order.orderDate} </p>
+                        </div>
                     </div>
-                    <div className={"order-time"}>
-                        <p> {order.orderTime} {order.orderDate} </p>
-
-                    </div>
-
-                </div>
-            )
-        }
-
-        <button onClick={() => {claimOrder(selectedOrders); setDoneSelecting(true)}}> DOE DINGEN TOEVOEGEN EN ZO</button>
+                )
+            }
+        </div>
+        <div className={"bottom-bar"}>
+            <div className={"claim-order-button"}>
+                <button onClick={() => {claimOrder(selectedOrders); setDoneSelecting(true)}}> DOE DINGEN TOEVOEGEN EN ZO</button>
+            </div>
+        </div>
     </div>
     )
 
