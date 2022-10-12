3b761b3e2aabdde737d020bf7e4d9a1f4e6406ac
maarten <maarten.kuijf@student.hu.nl>
2022-10-12 12:59:14 +0200

    time Of order in dto split in date & time

--- src/main/java/com/infosupport/happ/application/dto/OrderData.java
+++ src/main/java/com/infosupport/happ/application/dto/OrderData.java
@@ -9,18 +9,31 @@ import java.util.List;
 public class OrderData {
     public final Long id;
     public final int tableNr;
-    public final LocalDateTime timeOfOrder;
+    public final String orderDate;
+    public final String orderTime;
     public final PreperationStatus preperationStatus;
     public final List<Product> drinkProducts;
     public final List<Product> foodProducts;
 
     public OrderData(int tableNr, LocalDateTime timeOfOrder, PreperationStatus preperationStatus, List<Product> drinkProducts, List<Product> foodProducts, Long id) {
         this.tableNr = tableNr;
-        this.timeOfOrder = timeOfOrder;
+        this.orderDate = getOrderDateFormated(timeOfOrder);
+        this.orderTime = getOrderTimeFormated(timeOfOrder);
         this.preperationStatus = preperationStatus;
         this.drinkProducts = drinkProducts;
         this.foodProducts = foodProducts;
         this.id = id;
 
+    }
+    public String getOrderTimeFormated(LocalDateTime timeOfOrder){
+        String orderTimeString;
+        orderTimeString = (timeOfOrder.getHour() + ":" + timeOfOrder.getMinute());
+        return orderTimeString;
+    }
+
+    public String getOrderDateFormated(LocalDateTime timeOfOrder){
+        String orderDateString;
+        orderDateString = (timeOfOrder.getYear() + "-" + timeOfOrder.getMonthValue() + "-" + timeOfOrder.getDayOfMonth());
+        return orderDateString;
     }
 }
