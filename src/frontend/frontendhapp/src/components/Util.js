ab5c3f3a750e25242a5580329fe707ed489ed0ac
misherHU <misher.wedervoort@student.hu.nl>
2022-10-22 20:52:02 +0200

    getOccurance werkt in ShoppingCart.js

--- src/frontend/frontendhapp/src/components/Util.js
+++ src/frontend/frontendhapp/src/components/Util.js
@@ -1,14 +1,38 @@
-export default function countOccuranceProduct(list) {
+export default function getOccuranceProducts(list) {
     let productOccurances = new Map();
-    list.map((product) => {
-            if (productOccurances.has(product)) {
-                productOccurances.set(product, productOccurances.get(product) + 1)
-            } else {
-                productOccurances.set(product, 1)
-            }
 
+    list.map(product =>{
+        if (!isProductAlreadyInMap(productOccurances,product)){
+            productOccurances.set(product,getTargetAmountInList(product,list))
         }
-    )
+    })
     return productOccurances;
 }
+
+
+function getTargetAmountInList(target,list){
+    let count = 0;
+    list.map(product => {
+        if (productCompare(product,target)){
+            count += 1;
+        }
+    })
+    return count;
+}
+
+
+function isProductAlreadyInMap(map,product){
+    let productAlreadyInMap = false;
+    map.forEach(function(key, val){
+        if (productCompare(val,product)){
+            productAlreadyInMap = true;
+        }
+    });
+    return productAlreadyInMap;
+}
+
+function productCompare(obj1, obj2){
+    return obj1.name === obj2.name;
+}
+
 
--- src/frontend/frontendhapp/src/components/shoppingcart/ShoppingCart.js
+++ src/frontend/frontendhapp/src/components/shoppingcart/ShoppingCart.js
@@ -3,21 +3,17 @@ import ShoppingCartItem from "./ShoppingCartItem";
 import SubmitButton from "../submitData/SubmitButton";
 import {GetShoppingCart, PlaceOrder} from "../../urlMappings/TableRequests";
 import {Actions} from "../submitData/Actions";
-import countOccurances from "../Util"
+import getOccuranceProducts from "../Util"
 
 export default function ShoppingCart() {
     const [shoppingCart, setShoppingCart] = useState([]);
-    //const [productsAlreadyAdded, setProductsAlreadyAdded] = useState([])
 
 
     useEffect(() => {
 
-        GetShoppingCart(52)
+        GetShoppingCart(69)
             .then(res => {
                 setShoppingCart(res)
-
-                console.log(shoppingCart.length)
-                //console.log(countOccurances(shoppingCart))
             })
             .catch(err => {
                 console.log(err)
@@ -30,13 +26,13 @@ export default function ShoppingCart() {
             <div>
                 <h1 align="center">Shopping cart</h1>
             </div>
-            {shoppingCart.map((item, index) => {
-                    return (
-                        <span key={item.id}>
-                            <ShoppingCartItem productName={item.name} amount={1}/>
+            {Array.from(getOccuranceProducts(shoppingCart).keys()).map((item, index) =>{
+                return (
+                    <span key={item.id}>
+                            <ShoppingCartItem productName={item.name} amount={Array.from(getOccuranceProducts(shoppingCart).values())[index]}/>
                             <hr/>
                         </span>
-                    );
+                );
             })}
             <SubmitButton buttonText={"Order"} tableId={52} action={Actions.PLACE_ORDER}/>
         </div>
