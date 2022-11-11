b0e8d94c8b19f31b1192ae75847e702aec0ae152
Khaibar <khaibar.karimi@student.hu.nl>
2022-11-11 10:35:29 +0100

    administration page toegevoegd...

--- src/frontend/frontendhapp/src/pages/AdministrationPage.jsx
+++ src/frontend/frontendhapp/src/pages/AdministrationPage.jsx
@@ -9,15 +9,13 @@ export default function AdministrationPage() {
             <div className="staffModuleHomePagina_container">
                 <h1>Administration</h1>
                 <div className="container2">
-                    <Link className="button homeModule_bt_space" to="">Add Product</Link>
+                    <Link className="button homeModule_bt_space" to="">Create Product</Link>
                     <Link className="button homeModule_bt_space" to="">Delete Product</Link>
                     <Link className="button homeModule_bt_space" to="">Update Product</Link>
                     <Link className="button homeModule_bt_space" to="">Get All Products</Link>
                     <hr/>
-                    <Link className="button homeModule_bt_space" to="">Add Staff</Link>
-                    <Link className="button homeModule_bt_space" to="">Delete Staff</Link>
-                    <Link className="button homeModule_bt_space" to="">Update Staff</Link>
-                    <Link className="button homeModule_bt_space" to="">Get All Staff</Link>
+                    <Link className="button homeModule_bt_space" to="/createStaff">Create Staff</Link>
+                    <Link className="button homeModule_bt_space" to="/allStaffMembers">Get All Staff</Link>
                     <Link className="button position-absolute bottom-0 end-0 m-4" to="/staff">Log out</Link>
 
                 </div>
