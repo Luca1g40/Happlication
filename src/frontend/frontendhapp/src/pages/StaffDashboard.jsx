95567264fddd0c309f7b53a5d2d526d82c183cec
maarten <maarten.kuijf@student.hu.nl>
2022-11-11 14:05:32 +0100

    error fix

--- src/frontend/frontendhapp/src/pages/StaffDashboard.jsx
+++ src/frontend/frontendhapp/src/pages/StaffDashboard.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Logout from "../components/Logout"
-import "../styles/StaffDashboard.css"
-
 import FetchTablesSettedOnTrue from "../staff_components/FetchTablesSettedOnTrue";
 
 
