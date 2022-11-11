39b65c7d5be6d5e48197f05c875f5c174092e0cc
Khaibar <khaibar.karimi@student.hu.nl>
2022-11-11 10:28:37 +0100

    administration page toegevoegd

--- src/frontend/frontendhapp/src/App.js
+++ src/frontend/frontendhapp/src/App.js
@@ -11,6 +11,7 @@ import Staff from "./pages/HomePageStaff";
 import PersonalModule from "./pages/PersonalModule";
 import StaffDashboard from "./pages/StaffDashboard";
 import StaffModuleHomePagina from "./pages/StaffModuleHomePagina";
+import AdministrationPage from "./pages/AdministrationPage";
 
 function App() {
     return (
@@ -24,6 +25,7 @@ function App() {
             <Route path="claims" element={<PersonalModule/>}/>
             <Route path="staffDashboard" element={<StaffDashboard/>}/>
             <Route path="staffmodule" element={<StaffModuleHomePagina/>}/>
+            <Route path="administration" element={<AdministrationPage/>}/>
         </Routes>
     )
 
