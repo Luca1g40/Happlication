2c907e0ac98a7998bc44cc3558f7aafd688b9751
misherHU <misher.wedervoort@student.hu.nl>
2022-11-18 13:39:14 +0100

    ProductSubCategory service en controller WERKT!!!

---
+++ src/frontend/frontendhapp/src/components/productcategory/ViewProductCategory.js
@@ -0,0 +1,20 @@
+import ErrormeldingLabel from "../utils/ErrormeldingLabel";
+import React from "react";
+
+export default function IngredientForm(props){
+    return(props.ingredient===undefined) ? (
+        <div>
+            <ErrormeldingLabel text={props.errorMeldingText}/>
+            <label htmlFor={"name"}>Name: </label>
+            <input name={"name"} onChange={(event)=>props.handleChange(event)}/>
+        </div>
+
+    ) : (
+        <div>
+            <ErrormeldingLabel text={props.errorMeldingText}/>
+            <label htmlFor={"name"}>Name: </label>
+            <input name={"name"} disabled={props.disabled} onChange={(event)=>props.handleChange(event)} value={props.ingredient.name}/>
+        </div>
+
+    )
+}
