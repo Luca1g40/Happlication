85161f6f7835161e27c8952001f0900babc8c876
maarten <maarten.kuijf@student.hu.nl>
2023-01-13 13:17:27 +0100

    Updaten van staff in frontend is weer werkend

--- src/main/java/com/infosupport/happ/application/dto/SimpleStaffData.java
+++ src/main/java/com/infosupport/happ/application/dto/SimpleStaffData.java
@@ -9,10 +9,12 @@ public class SimpleStaffData {
     public final Long id;
     public final String name;
     public final List<Rights> rights;
+    public final int password;
 
-    public SimpleStaffData(Long id, String name, List<Rights> rights) {
+    public SimpleStaffData(Long id, String name, List<Rights> rights, int password) {
         this.id = id;
         this.name = name;
         this.rights = rights;
+        this.password = password;
     }
 }
